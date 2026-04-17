63014f877aa826c5e6a68b5efe368a314b16ddbf
Samuel De Francesco <defrancescodev@gmail.com>
2026-04-17 17:17:38 -0300

    b

--- src/main/java/com/barbershop/manager/controllers/UserController.java
+++ src/main/java/com/barbershop/manager/controllers/UserController.java
@@ -1,11 +1,11 @@
 package com.barbershop.manager.controllers;
 
 import com.barbershop.manager.models.DTOs.UserDTO;
+import com.barbershop.manager.models.DTOs.UserInsertDTO;
 import com.barbershop.manager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +25,12 @@ public class UserController {
     public UserDTO findById(Long id) {
         return userService.findById(id);
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO insert(@RequestBody UserInsertDTO userDTO) {
+        return this.userService.insert(userDTO);
+    }
+
+
 }
--- src/main/java/com/barbershop/manager/services/UserService.java
+++ src/main/java/com/barbershop/manager/services/UserService.java
@@ -1,19 +1,19 @@
 package com.barbershop.manager.services;
 
 import com.barbershop.manager.models.DTOs.UserDTO;
+import com.barbershop.manager.models.DTOs.UserInsertDTO;
 import com.barbershop.manager.models.entities.User;
 import com.barbershop.manager.models.exceptions.ResourceNotFoundException;
+import com.barbershop.manager.models.exceptions.RoleNullException;
 import com.barbershop.manager.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository userRepository;
 
@@ -30,4 +30,28 @@ public class UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
+    public User convertDTOtoEntity(UserInsertDTO userDTO) {
+        if (userDTO != null && userDTO.getRole() != null && userDTO.getEmail() != null) {
+            User user = new User();
+            user.setName(user.getName());
+            user.setEmail(userDTO.getEmail());
+            user.setId(userDTO.getId());
+            user.setRole(userDTO.getRole());
+            return user;
+        } else {
+            throw new RoleNullException("Role must not be null");
+        }
+
+    }
+
+    public UserDTO insert(UserInsertDTO dto) {
+        User user = convertDTOtoEntity(dto);
+        userRepository.save(user);
+        return new UserDTO(user);
+
+    }
+
 }
+
+
+
