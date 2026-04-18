ff6489b59c06f8b84b6e6d5a220e56b8047530a0
lucas.barroso <lucasbarrososainfinity@gmail.com>
2026-04-18 14:07:26 -0300

    Update: DataBaseException

---
+++ src/main/java/com/barbershop/manager/models/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.barbershop.manager.models.exceptions;
+
+public class DataBaseException extends RuntimeException {
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
