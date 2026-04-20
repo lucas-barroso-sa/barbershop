844735066d4e049b19488a9fdeaee2ec559b4f64
lucas.barroso <lucasbarrososainfinity@gmail.com>
2026-04-19 22:40:04 -0300

    refactor: atribute name and getters/setters

--- src/main/java/com/barbershop/manager/models/entities/Servicing.java
+++ src/main/java/com/barbershop/manager/models/entities/Servicing.java
@@ -11,20 +11,53 @@ public class Servicing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
     private String name;
     private double price;
-    private int duration;
-
-    @OneToMany
-    private List<Schedule> schedules = new ArrayList<>();
+    @Column(nullable = false)
+    private int durationInMinutes;
 
     public Servicing() {
 
     }
 
-    public Servicing(Long id, String name, double price) {
+    public Servicing(Long id, String name, double price , int durationInMinutes) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.durationInMinutes =  durationInMinutes;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int duration) {
+        this.durationInMinutes = duration;
     }
 }
