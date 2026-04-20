d4303480326373e72a8a1e6796279a51fd40e405
lucas.barroso <lucasbarrososainfinity@gmail.com>
2026-04-19 22:39:15 -0300

    update: ScheduleInsertDTO

---
+++ src/main/java/com/barbershop/manager/models/DTOs/ScheduleInsertDTO.java
@@ -0,0 +1,40 @@
+package com.barbershop.manager.models.DTOs;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ScheduleInsertDTO {
+    @NotNull(message = "The Schedule time is mandatory")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime appointmentTime;
+
+    @NotNull(message = "Client Id is mandatory")
+    private Long clientId;
+
+    @NotNull(message = "User Id is mandatory")
+    private Long userId;
+
+    @NotEmpty(message = "must have at least one servicing")
+    private List<Long> servicingIds;
+
+    public ScheduleInsertDTO() {
+    }
+
+    // Getters e Setters
+    public LocalDateTime getAppointmentTime() { return appointmentTime; }
+    public void setAppointmentTime(LocalDateTime appointmentTime) { this.appointmentTime = appointmentTime; }
+
+    public Long getClientId() { return clientId; }
+    public void setClientId(Long clientId) { this.clientId = clientId; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public List<Long> getServicingIds() { return servicingIds; }
+    public void setServicingIds(List<Long> servicingIds) { this.servicingIds = servicingIds; }
+
+}
